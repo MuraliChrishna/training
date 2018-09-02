0e7f5445ea1c5be7c476ecfa1ff3dc67f5a3aee9
Bala Murali Krishna Polisetti <muralikrishna4400@hotmail.com>
2018-09-02 15:25:05 -0400

    Assignmnet on event data and two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-servers',
@@ -6,22 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+emptyString = false;
 allowNewServer = false;
+userName = '';
+usernameStatus = 'clcik to update username';
 sercerCreationStatus = 'no Server Was Created';
 serverName = 'TestServer';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+    if (this.userName = '') {
+      this.emptyString = true;
+      }
   }
-
   ngOnInit() {
   }
-
   onCreateServer() {
-this.sercerCreationStatus = 'server was Created Name is '+ this.serverName;
+this.sercerCreationStatus = 'server was Created Name is ' + this.serverName;
+  }
+  onUserName() {
+this.usernameStatus = 'new username is ' + this.userName;
   }
 onUpdateServerName(event: any) {
 this.serverName = (<HTMLInputElement>event.target).value;
 }
+onUpateUserName(event: any) {
+this.userName = (<HTMLInputElement>event.target).value;
+}
+
 }
