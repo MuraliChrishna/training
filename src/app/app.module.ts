f3d96cee5cc71eba249e8f3899c5eb73eb1fbd08
Bala Murali Krishna Polisetti <muralikrishna4400@hotmail.com>
2018-09-01 22:55:56 -0400

    functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
+import { SuccessAlertComponent } from './Successalert/success-alert.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+allowNewServer = false;
+sercerCreationStatus = 'no Server Was Created';
+serverName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+this.sercerCreationStatus = 'server was Created';
+  }
+onUpdateServerName(event: any) {
+this.serverName = (<HTMLInputElement>event.target).value;
+}
 }
